ebf3ff18af412dd3bf8930e30651c8a542e86383
Sangyun Han <sangyun628@gmail.com>
2018-04-27 10:04:52 +0900

    update creation and deletion

--- vpn/strongswan.py
+++ vpn/strongswan.py
@@ -2,57 +2,122 @@ import os
 import sys
 import subprocess
 import copy
+from collections import OrderedDict
 
 
 # path of ipsec.conf
 IPSEC_CONFIG_PATH = "/etc/ipsec.conf"
 IPSEC_SECRETS_PATH = "/etc/ipsec.secrets"
+IPSEC_CONFIG_FILE = '### ipsec.conf ###\nconfig setup\n\tcharondebug="all"\n\tuniqueids=yes\n\tstrictcrlpolicy=no\n\n'
+IPSEC_SECRETS_FILE = '### ipsec.secrets ###\n\n'
 TUNNEL_CONFIG_KEY = ["conn", "left", "leftsubnet", "right", "rightsubnet", "ikelifetime", "lifetime", "keyexchange", "dpddelay", "dpdtimeout", "ike", "esp", "keyingtries""dpdaction", "authby", "auto", "type"]
+TUNNEL_CONFIG_LINE = len(TUNNEL_CONFIG_KEY)
 TUNNEL_CONFIG_PARAMETER_COUNT = 8
-TUNNEL_CONFIG_FORMAT = "###TUNNEL_START###\nconn %s\n\tleft=%s\n\tleftsubnet=%s\n\tright=%s\n\trightsubnet=%s\n\tikelifetime=%s\n\tlifetime=%s\n\tkeyexchange=%s\n\t"
-DEFAULT_CONFIG = "dpddelay=30\n\tdpdtimeout=120\n\tike=aes256-sha2_256-modp1024!\n\tesp=aes256-sha2_256!\n\tkeyingtries=0\n\tdpdaction=restart\n\tauthby=secret\n\tauto=start\n\ttype=tunnel\n###TUNNEL_END###\n\n"
+TUNNEL_CONFIG_FORMAT = "\nconn %s\n\tleft=%s\n\tleftsubnet=%s\n\tright=%s\n\trightsubnet=%s\n\tikelifetime=%s\n\tlifetime=%s\n\tkeyexchange=%s\n\t"
+DEFAULT_CONFIG = "dpddelay=30\n\tdpdtimeout=120\n\tike=aes256-sha2_256-modp1024!\n\tesp=aes256-sha2_256!\n\tkeyingtries=0\n\tdpdaction=restart\n\tauthby=secret\n\tauto=start\n\ttype=tunnel\n\n"
+
+
+def initConfigFile():
+    # init ipsec.conf file
+    f = open(IPSEC_CONFIG_PATH, "w")
+    f.write(IPSEC_CONFIG_FILE)
+    f.close()
+
+    #init ipsec.secrets
+    f = open(IPSEC_SECRETS_PATH, "w")
+    f.write(IPSEC_SECRETS_FILE)
+    f.close()
 
 def parseConfig(configStr):
     configList = configStr.split(",")
     return configList
 
-def setTunnel(tunnelConfigList):
+def setTunnel(tunnelConfigList, psk):
+    if len(tunnelConfigList) != TUNNEL_CONFIG_LINE:
+        return 0
+
+    tunnelConfigDict = OrderedDict(zip(TUNNEL_CONFIG_KEY, tunnelConfigList))
     tunnelConfigStr = copy.deepcopy(TUNNEL_CONFIG_FORMAT)
     tunnelConfigStr = tunnelConfigStr % tuple(tunnelConfigList)
-    tunnelId = tunnelConfigList[0]
-
-    #print(tunnelConfigStr+DEFAULT_CONFIG)
     tunnelConfigStr = tunnelConfigStr + DEFAULT_CONFIG
-    
+
+    # TODO 중복 체크 추가
     f = open(IPSEC_CONFIG_PATH, "a")
     f.write(tunnelConfigStr)
     f.close()
 
+    result = subprocess.check_output("ipsec update", shell=True)
+    print(result)
+
+    # add ###TUNNEL_ID###
+    # add PSK 192.168.1.101 192.168.1.102 : PSK 'test123'
+    secret = tunnelConfigDict["left"] + " " + tunnelConfigDict["right"] + " : PSK " + "'%s'"%(psk) + "\n\n"
     f = open(IPSEC_SECRETS_PATH, "a")
+    f.write("###%s###\n"%(tunnelConfigDict["conn"]))
+    f.write(secret)
+    f.close()
+
+    #tunnelId = tunnelConfigList[0]
+    #result = subprocess.check_output("ipsec up %s"%(tunnelId), shell=True)
+    #print(result)
+
+def deleteTunnel(tunnelId):
+    # delete tunnel
+    f = open(IPSEC_CONFIG_PATH, "r")
+    tunnel_lines = f.readlines()
+    f.close()
+
+    for i in range(len(tunnel_lines)):
+        if ("conn " + tunnelId) in tunnel_lines[i]:
+            del tunnel_lines[i:i+TUNNEL_CONFIG_LINE+2]
+            break
+
+    f = open(IPSEC_CONFIG_PATH, "w")
+    f.writelines(tunnel_lines)
+    f.close()
+
+
+    # delete secrets
+    f = open(IPSEC_SECRETS_PATH, "r")
+    secret_lines = f.readlines()
+    f.close()
+
+    for i in range(len(secret_lines)):
+        if "###%s###"%(tunnelId) in secret_lines[i]:
+            del secret_lines[i:i+2]
+            break
+    f = open(IPSEC_SECRETS_PATH, "w")
+    f.writelines(secret_lines)
+    f.close()
 
 
     result = subprocess.check_output("ipsec update", shell=True)
     print(result)
-    #result = subprocess.check_output("ipsec up %s"%(tunnelId), shell=True)
-    #print(result)
+    result = subprocess.check_output("ipsec down %s"%(tunnelId), shell=True)
+    print(result)
+
+    # delete PSK
+
 
 
 if __name__ == "__main__":
     print("Number of arguments : ", len(sys.argv))
     print("Argument List : ", str(sys.argv))
-    print(sys.argv[1])
-    tempConfig = parseConfig(sys.argv[1])
-    setTunnel(tempConfig)
+    for i in len(sys.argv):
+        print(sys.argv[i])
 
-'''    
     command = sys.argv[1]
 
     if command == "create":
-        setTunnel(sys.argv[2])
+        config = parseConfig(sys.argv[2])
+        setTunnel(config)
     elif command == "update":
-        pass
+        config = parseConfig(sys.argv[2])
+        tunnelId = config[0]
+
     elif command == "delete":
-        pass
+        deleteTunnel(sys.argv[2])
+    elif command == "init":
+        initConfigFile()
     else:
         print("ERROR")
-''' 
